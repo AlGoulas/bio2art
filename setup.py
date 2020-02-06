7ddd2039ae57c274d2e1b63a73543121bcdf298d
AlGoulas <50454215+AlGoulas@users.noreply.github.com>
2020-02-06 15:40:24 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 14:50:47 2020
+
+@author: alexandrosgoulas
+"""
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='bio2art',
+      version='0.1',
+      description='Convert biological to artifial networks',
+      author='Alexandros Goulas',
+      author_email='agoulas227@gmail.com',
+      url='',
+      license='',
+      install_requires=[],
+      classifiers=[
+          'Intended Audience :: Science/Research/Any',
+          'Programming Language :: Python :: 3.7.3',
+      ],
+      packages=find_packages())
