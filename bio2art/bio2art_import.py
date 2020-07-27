0fb8f61c0ca392e653af9f9b98c2338f1a3740df
AlexGoulas <agoulas227@gmail.com>
2020-07-27 16:00:07 +0200

    weight partition and sparsity options

--- bio2art/bio2art_import.py
+++ bio2art/bio2art_import.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import csv
+import random
+
+from bio2art import bio2art_utils
 
 # A set of functions for converting biological neuronal networks to artificial
 # neuronal networks. The output is a conenctivity matrix (2D numpy array) that 
@@ -14,11 +17,13 @@ def bio2art_from_list(path_to_connectome_folder, file):
     """
     Generate matrix W from scv file
     
-    input:
+    Input
+    -----
     path_to_connectome_folder: the path to the folder with the csv file
     file: the name of the csv file
     
-    output:
+    Output
+    ------
     W: the connectivity matrix in the form of a numpy array
     
     """
@@ -95,12 +100,13 @@ def bio2art_from_list(path_to_connectome_folder, file):
 def bio2art_from_conn_mat(
         path_to_connectome_folder, 
         file_conn, 
-        neuron_density=None, 
-        seed_neurons=None, 
-        intrinsic_conn=True, 
-        target_sparsity=0.2, 
-        intrinsic_wei=0.8,
-        keep_diag=True
+        neuron_density = None, 
+        seed_neurons = None, 
+        intrinsic_conn = True, 
+        target_sparsity = 0.2, 
+        intrinsic_wei = 0.8, 
+        rand_partition = False,
+        keep_diag = True
         ):
     
     """
@@ -111,61 +117,84 @@ def bio2art_from_conn_mat(
     path_to_connectome_folder: the path to the folder with connectome files
     
     file: string with the name of the connectome file of the connectome you 
-    would like to use. Currently available:
-    Drosophila                     49x49 (shape of the npy array)
-    Human_Betzel_Normalized        57x57 
-    Macaque_Normalized             29x29
-    Marmoset_Normalized            55x55
-    Mouse_Gamanut_Normalized       19x19
-    Mouse_Ypma_Oh.npy              56x56
+        would like to use. Currently available:
+        
+        'Drosophila'                     49x49 (NxN shape of the npy array)
+        'Human_Betzel_Normalized'        57x57 
+        'Macaque_Normalized'             29x29
+        'Marmoset_Normalized'            55x55
+        'Mouse_Gamanut_Normalized'       19x19
+        'Mouse_Ypma_Oh'                  56x56
     
-    neuron_density: numpy array of positive integers withshape N where 
-    N network_original.shape[0] with network_original 
-    the actual biological connectome (above). Each entry of neuron_density[i] 
-    is denoting the number of neurons that we assume to inhabit region i. 
-    neuron_density by default gets populated with 1s. Note that if seed_neurons 
-    is not None each entry of neuron_density will be normalized as proportion 
-    over the sum(neuron_density)
+    neuron_density: numpy array of positive integers with shape N where 
+        N network_original.shape[0] with network_original 
+        the actual biological connectome (above). Each entry of 
+        neuron_density[i] is denoting the number of neurons that we assume 
+        to inhabit region i. 
+        neuron_density by default gets populated with 1s (1 neuron per region). 
     
-    seed_neurons: Positive integer denoting the nr of neurons that will be 
-    multiplied by neuron_density[i] to result in the number of neurons to be 
-    considered for each region i. 
-    Default None. Note that if seed_neurons=None, the neuron_density array is 
-    not scaled and used as is.
+        Note that if seed_neurons is not None each entry of neuron_density 
+        will be normalized as proportion over the sum(neuron_density)
     
-    intrinsic_conn: Boolean denoting if the within regions neuron-to-neuron
-    conenctivity will be generated. Note that currently all-to-all
-    within region conenctions are assumed and implemented. Default True. 
+    seed_neurons: Positive integer, default None, denoting the nr of neurons 
+        that will be multiplied by neuron_density[i] to result in the number 
+        of neurons to be considered for each region i. 
     
-    target_sparsity: float (0 1] for each source neuron the percentage of all 
-    possible neuron-targets to form connections with. Note that at least 1 
-    neuron will function as target in case that the resulting percentage is <1.
-    This parameter can be used to make the sparisty of network_scaled vary
-    around the density dictated by the actual biological connectomes.
-    Default=0.2. Note that this parameter is meaningful only if at least one 
-    region has more than 1 neuron, that is, for some i, neuron_density[i]>1.
+        The neuron_density numpy array will be normalized for every region i as:  
+        neuron_density[i] / sum(neuron_density)
     
-    intrinsic_wei: float (0 1] denoting the percentage of the weight that 
-    will be assigned to the intrinsic weights. E.g., 0.8*sum(extrinsic weight)
-    where sum(extrinsic weight) is the sum of weights of connections from 
-    region A to all other regions, but A.
+        Note that if seed_neurons=None, the neuron_density array is 
+        not scaled and used as is.
     
-    keep_diag: Boolean variable denoting if the diagonal entries (denoting 
-    self-to-self neuron connections) should be kept of or not. Default True. 
-    Note that this parameter only has an effect  when intrinsic_conn is True.
+    intrinsic_conn: Boolean, default True denoting if the within regions 
+        neuron-to-neuron  connectivity will be generated. 
+        Note that currently all-to-all within region connections are 
+        assumed and implemented. 
+    
+    target_sparsity: float (0 1], default 0.2, for each source neuron the 
+        percentage of all possible neuron-targets to form connections with. 
+        Note that at least 1 neuron will function as target in case that the 
+        resulting percentage is <1.
+        This parameter can be used to make the sparisty of network_scaled vary
+        around the density dictated by the actual biological connectomes.
+        Note that this parameter is meaningful only if at least one region 
+        has more than 1 neuron, that is, for some i, neuron_density[i]>1.
+    
+    intrinsic_wei: float (0 1], default 0.8, denoting the percentage of the
+        weight that will be assigned to the intrinsic weights. 
+        E.g., 0.8*sum(extrinsic weight)where sum(extrinsic weight) is the sum 
+        of weights of connections from region A to all other regions, but A.
+    
+    rand_partition: Boolean, default False, specifying if the original weight
+        of each connection in the empirical connectome will be partitioned
+        in k parts that sum to the original connection weight, 
+        where k = nr_source_neurons * nr_target_neurons. nr_source_neurons
+        and nr_target_neurons are the nr of neurons in the source i and target 
+        areas j as part of C_Neurons[i,j]. The original connection weight is
+        C[i,j] where C the connectome corresponding to the dataset specified
+        on the parameter file.
+        If False, then for a given source and target i,j C[i,j] will
+        be populated withe equal constant wight values such that:
+        C_Neurons[m,n] = C[i,j] / (nr_source_neurons * nr_target_neurons)
+        with m, n all index neurons beloning to region i, j respectively. 
+    
+    keep_diag: Boolean variable, default True denoting if the diagonal entries 
+        (denoting self-to-self neuron connections) should be kept of or not. 
+        Note that this parameter only has an effect when intrinsic_conn is True.
     
     Output
     ------
-    network_original: The actual biological connectome that was used in the 
-    form of a numpy array
+    network_original: The actual biological neural network that was used in the 
+        form of a NxN numpy array (see description of data sets for each N)
     
-    network_scaled: the artificial neuronal network in the form of a 
-    numpy array
+    network_scaled: the artificial neural network in the form of a 
+        numpy array (NxN with N bound to the parameters seed_neurons,
+        neuron_density)
     
     region_neuron_ids: A list of list of integers for tracking the neurons of 
-    the network_scaled array. region_neuron_ids[1] contains a list with 
-    integers that denote the neurons of region 1 in network_scaled as 
-    network_scaled[region_neuron_ids[1],region_neuron_ids[1]]
+        the network_scaled array. region_neuron_ids[1] contains a list with 
+        integers that denote the neurons of region 1 in network_scaled as 
+        network_scaled[region_neuron_ids[1],region_neuron_ids[1]]
     
     """
     file_conn = 'C_' + file_conn + '.npy' # Prefix and suffix for the file
@@ -234,7 +263,7 @@ def bio2art_from_conn_mat(
     # We use the region_neuron_ids and the weight value of the region-to-region
     # matrix network_original.
     
-    # Start populating by row of the region-to region matrix network_original
+    # Start populating by row of the region-to-region matrix network_original
     for i in range(network_original.shape[0]):
         
         # not-zeros denote the indexes of the areas that are receiving
@@ -245,54 +274,103 @@ def bio2art_from_conn_mat(
         sources_indexes = region_neuron_ids[i]
         
         if intrinsic_conn is True:
-            # Add an intrinsic within region weight by interconencting all the 
+            # Add an intrinsic within-region weight by interconnecting all the 
             # neurons that belong to one region
             
             # Intrinsic weight of within region - default 80%
             intrinsic_weight = (intrinsic_wei * sum_C_out[i]) / (1-intrinsic_wei) 
             
+            # If target sparsity is specified then calculate the percentage 
+            # of intrinsic targets to use (source_indexes)
+            sources_indexes_to_use = list(range(len(sources_indexes)))
+            nr_sources_to_use = target_sparsity*len(sources_indexes) 
+            
+            # Ensure that we keep at least one target neuron
+            if nr_sources_to_use < 1:
+                nr_sources_to_use = 1
+            else:
+                nr_sources_to_use = int(np.round(nr_sources_to_use))
+            
+            # Keep random nr_sources_to_use
+            random.shuffle(sources_indexes_to_use)
+            sources_indexes_to_use = sources_indexes_to_use[:nr_sources_to_use]
+            
+            if rand_partition:
+                partitioned_weights = bio2art_utils.float_partition(intrinsic_weight, 
+                                                                   len(sources_indexes) * nr_sources_to_use)
+                # Make the idx to use the different partitioned_weights
+                # when connecting sources to targets 
+                start_partitioned_weights=0
+                stop_partitioned_weights=nr_sources_to_use    
+            else:   
+                # Normalize weight based on the number of target given the 
+                # target_sparsity parameter
+                intrinsic_weight = intrinsic_weight / (len(sources_indexes) * nr_sources_to_use)
+                       
             # Populate the matrix with broadcasting of indexes
-            for sources in range(len(sources_indexes)):
-                network_scaled[sources_indexes[sources], sources_indexes] = intrinsic_weight
-            
-            
+            for sources in sources_indexes:
+                if rand_partition:
+                    network_scaled[sources, 
+                                   np.asarray(sources_indexes)[sources_indexes_to_use]] = partitioned_weights[start_partitioned_weights:stop_partitioned_weights]
+                    # Update idx for using the partitioned_weights
+                    start_partitioned_weights = stop_partitioned_weights
+                    stop_partitioned_weights = stop_partitioned_weights + nr_sources_to_use
+                else:
+                    network_scaled[sources, 
+                                   np.asarray(sources_indexes)[sources_indexes_to_use]] = intrinsic_weight
+                        
         # Loop through the not zeros indexes and fetch the target neuron 
         # Ids that are stored in region_neuron_ids
-        for target in range(len(not_zeros)):
-            target_indexes = region_neuron_ids[not_zeros[target]]
+        for target in not_zeros:
+            target_indexes = region_neuron_ids[target]
             
             # Calculate here the strength of connectivity that should be 
             # assigned to the neuron-to-neuron matrix.
             # The weight is dictated by the number of source and target neurons
             # and the respective region-to-region weight of matrix network_original
-            current_weight = network_original[i, not_zeros[target]]
+            current_weight = network_original[i, target]
             
-            neuron_to_neuron_weight = current_weight / (len(sources_indexes)*len(target_indexes))
+            # Here we can control the sparsity of connections by choosing
+            # the portion of all target_indexes to be used.
+            # Hence, apply the target_sparsity parameter
+            nr_targets_to_use = target_sparsity*len(target_indexes) 
             
-            # For now the neuron-to-neuron weight is identical due to 
-            # lack of the precise number from experimental observations.
-            # It migh be needed to inject soem noise for variations to emerge.
+            # Ensure that we keep at least one target neuron
+            if nr_targets_to_use < 1:
+                nr_targets_to_use = 1
+            else:
+                nr_targets_to_use = int(np.round(nr_targets_to_use))
+            
+            # Keep random nr_targets_to_use
+            target_indexes_to_use = np.random.permutation(len(target_indexes))
+            target_indexes_to_use = target_indexes_to_use[:nr_targets_to_use]
+            target_indexes = np.asarray(target_indexes)[target_indexes_to_use] 
+            
+            if rand_partition:
+                partitioned_weights = bio2art_utils.float_partition(current_weight, 
+                                                                   len(sources_indexes) * nr_targets_to_use)
+                # Make the idx to use the different partitioned_weights
+                # when connecting sources to targets 
+                start_partitioned_weights=0
+                stop_partitioned_weights=nr_targets_to_use    
+            else:   
+                # Normalize weight based on the number of target given the 
+                # target_sparsity parameter
+                neuron_to_neuron_weight = current_weight / (len(sources_indexes) * nr_targets_to_use)
+                        
+            # This weight normalization is relying on a fixed nr of source and 
+            # target neurons and does not consider the target_sparsity param  
+            #neuron_to_neuron_weight = current_weight / (len(sources_indexes)*len(target_indexes))
             
             # Populate the matrix with broadcasting of indexes
-            for sources in range(len(sources_indexes)):
-                
-                # Here we can control the sparsity of connections by choosing
-                # the portion of all target_indexes to be used.
-                # Hence, apply the target_sparsity parameter
-                nr_targets_to_use = target_sparsity*len(target_indexes)
-                
-                # Ensure that we keep at least one target neuron
-                if nr_targets_to_use < 1:
-                    nr_targets_to_use = 1
+            for sources in sources_indexes: 
+                if rand_partition:
+                    network_scaled[sources, target_indexes] = partitioned_weights[start_partitioned_weights:stop_partitioned_weights]
+                    # Update idx for using the partitioned_weights
+                    start_partitioned_weights = stop_partitioned_weights
+                    stop_partitioned_weights = stop_partitioned_weights + nr_targets_to_use 
                 else:
-                    nr_targets_to_use = int(np.round(nr_targets_to_use))
-                
-                # Keep random nr_targets_to_use
-                target_indexes_to_use = np.random.permutation(len(target_indexes))
-                target_indexes_to_use = target_indexes_to_use[0:nr_targets_to_use]
-                target_indexes = np.asarray(target_indexes)[target_indexes_to_use]            
-                
-                network_scaled[sources_indexes[sources], target_indexes] = neuron_to_neuron_weight
+                    network_scaled[sources, target_indexes] = neuron_to_neuron_weight
                 
     # Remove self-to-self strength/connections (diagonal of network_scaled) 
     if keep_diag is False:    
--- bio2art/bio2art_utils.py
+++ bio2art/bio2art_utils.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import pickle 
+import numpy as np
+import random
 
-# Return the names of the regions of the indicated connectome
+# Load the names of the regions of the indicated connectome
 def get_names(path_to_connectome_folder, data_name):
     '''
+    Load the names of the regions of the indicated dataset
+    
     Input
     -----
     path_to_connectome_folder: the path to the folder with connectome files
     
     data_name: string with the name of the connectome file of the connectome you 
-    would like to use. Currently available:
+        would like to use. Currently available:
+        
     Drosophila                     
     Human_Betzel_Normalized        
     Macaque_Normalized             
@@ -21,7 +26,7 @@ def get_names(path_to_connectome_folder, data_name):
     Output
     ------
     names: list of strings of len N, with N the number of areas in the 
-    connectome, indicating the names of each brain region
+        connectome, indicating the names of each brain region/node
     
     Note: Pickle is used for loading the names
     
@@ -31,4 +36,163 @@ def get_names(path_to_connectome_folder, data_name):
     with open(file_to_open, 'rb') as fp: 
         names = pickle.load(fp)
     
-    return names 
+    return names
+ 
+#  Load the neuron density of the regions of the indicated dataset
+def get_neuron_density(path_to_connectome_folder, data_name):
+    '''
+    Load the neuron density of the regions of the indicated dataset
+    
+    Input
+    -----
+    path_to_connectome_folder: the path to the folder with connectome files
+    
+    data_name: string with the name of the connectome file of the connectome you 
+        would like to use. Currently available:
+              
+    Macaque_Normalized             
+    Marmoset_Normalized            
+
+    Output
+    ------
+    neuron_density: list of strings of len N, with N the number of areas in the 
+        connectome, indicating the names of each brain region/node
+    
+    '''
+    
+    file_conn = 'ND_' + data_name + '.npy' # Prefix and suffix for the file
+    file_to_open = path_to_connectome_folder / file_conn
+    neuron_density = np.load(file_to_open)
+    
+    return neuron_density 
+
+#  Construct a scaled neuron_density array based on the seed_neuron
+def scale_neuron_density(neuron_density, 
+                         seed_neuron=1,
+                         scale_type='rank'):
+    '''
+    Construct a scaled neuron_density array based on the seed_neuron
+     
+    Input
+    -----
+    neuron_density: numpy array of shape (N,), with N the number of areas in 
+        the connectome with entry i denoting the neuron denisty of region i 
+        (returned from function get_neuron_density).
+    
+    seed_neuron: int, default 1, specifying the number that will be multiplied
+        by the scaled neuron_density.
+        
+    scale_type: string 'ratio' 'rank', default 'rank', specifying how the 
+        neuron_density valeus will be scaled. 
+        'rank': the values are rank ordered and multiplied by seed_neuron
+        'ratio':the values are converted to ratios, 
+        neuron_density[i] / min(neuron_density) and multiplied by seed_neuron 
+       
+    
+    Output
+    ------
+    scaled_neuron_density: numpy array of shape (N,), with N the number  
+        of areas in the connectome, indicating the scaled neuron_density of 
+        each brain region/node
+    
+    '''
+    # Copy and sort the neuron densities
+    neuron_density_srt = neuron_density.copy()
+    neuron_density_srt.sort()
+    
+    # Get the idx so you can go match neuron_density_srt to neuron_density 
+    # The y_ind will contain the desired idx such that:
+    # neuron_density[y_ind[i]] == nd_sorted[i] for every i=0,1,2...N 
+    # with N=neuron_density.shape[0] 
+    xy, x_ind, y_ind = np.intersect1d(neuron_density_srt, 
+                                      neuron_density, 
+                                      return_indices=True)
+    
+    scaled_neuron_density = neuron_density.copy()
+    
+    if scale_type == 'rank':
+        scaled_neuron_density[y_ind] = np.asarray(range(1, neuron_density.shape[0]+1)
+                                                 ) * seed_neuron
+    elif scale_type == 'ratio':
+        for i, item in enumerate(neuron_density_srt):
+            if i == 0: 
+                scaled_neuron_density[y_ind[i]] = 1
+                denominator = neuron_density_srt[i]
+            scaled_neuron_density[y_ind[i]] = round((item / denominator) * seed_neuron)       
+        
+    scaled_neuron_density = scaled_neuron_density.astype('int64')                                  
+    
+    return scaled_neuron_density 
+
+# Partition integer i in n random integers that sum to i 
+def int_partition(i, n):
+    '''
+    Partition integer i in n random integers that sum to i 
+    
+    Input
+    -----
+    i:  int, a positive integer that needs to be partitioned in n integers
+        that sum to i 
+    n:  int, a positive integer specifying the number of partitions/integers
+        to be generated
+         
+    Output
+    ------   
+    partitions, list, list of n integers with the property 
+        sum(partitions)==i 
+    '''
+    partitions=[]
+    if n > i:
+        print('\nInteger i must be higher or equal to n\n') 
+        return
+    if i < 0 or n < 0:
+        print('\nIntegers i and n must be positive\n') 
+        return
+             
+    spectrum = (i-n)+2
+    for k in range(n-1):
+        if spectrum > 1:
+            new_val = np.random.randint(1, high=spectrum)
+            partitions.append(new_val)
+            spectrum = spectrum-new_val
+        else:
+            new_val = np.random.randint(1, high=2)
+            partitions.append(new_val)
+   
+    partitions.append(i-sum(partitions))
+    # Shuffle list so that the order of generation does not bias magnitude 
+    # of integers as they are generated
+    random.shuffle(partitions)
+    
+    return partitions
+
+# Partition float f in n random floats that sum to f 
+def float_partition(f, n):
+    '''
+    Partition float f in n random floats that sum to f 
+    
+    Input
+    -----
+    f:  float, a positive float that needs to be partitioned in n floats
+        that sum to i 
+    n:  int, a positive integer specifying the number of partitions/integers
+        to be generated
+         
+    Output
+    ------   
+    partitions, list, list of n float numbers with the property 
+        sum(partitions)==f
+    '''
+    partitions=[]             
+    spectrum = f
+    for k in range(n-1):
+        new_val = random.uniform(0., abs(spectrum))
+        partitions.append(new_val)
+        spectrum = spectrum-new_val
+   
+    partitions.append(f-sum(partitions))
+    # Shuffle list so that the order of generation does not bias magnitude 
+    # of integers as they are generated
+    random.shuffle(partitions)
+    
+    return partitions            
